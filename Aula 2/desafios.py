181a9230f2175aac0c1ba21b0d6070e074e01b03
EnzoHino <enzoyudihino@gmail.com>
2026-03-16 22:41:50 -0300

    variaveis

---
+++ Aula 2/desafios.py
@@ -0,0 +1,19 @@
+# desafio 1
+nome = input('Digite seu nome: ')
+
+print(f"\nSeja bem-vindo, {nome}")
+print("\n==============================================\n")
+
+# desafio 2
+dia = int(input('Digite o dia de seu nascimento: '))
+mes = int(input('Digite o mês de seu nascimento: '))
+ano = int(input('Digite o ano de seu nascimento: '))
+
+print(f"\nSua data de nascimento é: {dia}/{mes}/{ano}")
+print("\n==============================================\n")
+
+# desafio 3
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
+
+print(f"\nA soma entre os números é: {n1 + n2}")
